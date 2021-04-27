75df9ee89cf76fb3e1d3754db1905e6532654434
Abel Guillen <abelazo@gmail.com>
2021-04-27 10:16:49 +0200

    Make homeconfig usable

--- scripts/self/utils/homeconfig.sh
+++ scripts/self/utils/homeconfig.sh
@@ -4,8 +4,8 @@ pushd $HOME >& /dev/null
 for configurationFile in `find ${DOTFILES_PATH} -name "DOT*" -exec greadlink -f '{}' \;`; do
     targetFilename=$(basename ${configurationFile} | sed -e 's/DOT/./')
     echo ${targetFilename}
-    #ln -sf ${configurationFile} ${targetFilename}
+    ln -sf ${configurationFile} ${targetFilename}
 done
 popd >& /dev/null
 
-#find ~ -name '.*' -type l -maxdepth 1 -ls
+find ~ -name '.*' -type l -maxdepth 1 -ls
