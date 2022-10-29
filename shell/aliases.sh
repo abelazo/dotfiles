4cfc3827de42418f178e0bf21f3e011e3acb6e1d
Abel Guillen <abelazo@gmail.com>
2022-10-29 12:13:14 +0200

    Remove preview alias as it is already covered by dot fs catdir

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -2,9 +2,6 @@ alias emacs='emacs -nw'
 alias ls='ls -G'
 alias tmp='cd $HOME/tmp'
 
-# Preview all files in the current subtree 
-alias preview="fzf --preview 'bat --color \"always\" {}'"
-
 # Docker
 alias dc_stopall='docker stop $(docker container ls -a -q)'
 alias dc_rmall='docker rm $(docker container ls -a -q)'
