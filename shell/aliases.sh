2279309023565067e3f9ec3df7d2ea20f029d9b5
Abel Guillen <abelazo@gmail.com>
2024-01-04 09:46:44 +0100

    feat(shell): Add a bunch of bash aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,13 +1,31 @@
+# New commands
+alias please='sudo'
+alias sha1='openssl sha1'
+alias path='echo -e ${PATH//:/\\n}'
+alias now='date +"%d-%m-%Y %T"'
+alias nowtime='date +"%T"'
+alias nowdate='date +"%d-%m-%Y"'
+
+# Command options
 alias emacs='emacs -nw'
 alias ls='ls -G'
+alias mkdir='mkdir -pv'
+
+# Routes
 alias tmp='pushd $HOME/tmp'
 
-# Docker
-alias dc_stopall='docker container stop $(docker container ls -a -q)'
-alias dc_rmall='docker container rm $(docker container ls -a -q)'
-alias di_rmall='docker image rm $(docker image ls -a -q)'
-alias ds_prune='docker system prune -a -f'
-alias dv_prune='docker volume prune -f'
+# Safety nets
+# do not delete / or prompt if deleting more than 3 files at a time
+alias rm='rm -I'
+# confirmations if command overwrites an existing file
+alias mv='mv -i'
+alias cp='cp -i'
+alias ln='ln -i'
+
+## Colorize the grep command output for ease of use (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
 
 # Git
 # alias gaa="git add -A"
@@ -21,6 +39,13 @@ alias dv_prune='docker volume prune -f'
 # alias gpsf="git push --force"
 # alias gpl="git pull --rebase --autostash"
 # alias gl="dot git pretty-log"
+
+# Docker
+alias dc_stopall='docker container stop $(docker container ls -a -q)'
+alias dc_rmall='docker container rm $(docker container ls -a -q)'
+alias di_rmall='docker image rm $(docker image ls -a -q)'
+alias ds_prune='docker system prune -a -f'
+alias dv_prune='docker volume prune -f'
 
 # Kubernetes
 alias kctx='kubectx'
