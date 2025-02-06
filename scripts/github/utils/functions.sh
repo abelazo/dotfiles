8663af57e29d7f23002b2664d0522b6553f95cad
Abel Guillen <abelazo@gmail.com>
2025-02-06 15:26:55 +0100

    feat(github): Add Pull request status and age

--- scripts/github/utils/functions.sh
+++ scripts/github/utils/functions.sh
@@ -1,4 +1,4 @@
-function wait_for_workflow {
+function github::wait_for_workflow {
   worflow_file=${1}
 
   c=1
@@ -8,4 +8,10 @@ function wait_for_workflow {
     ((c++)) && ((c==10)) && break
     sleep 1
   done
+}
+
+function github::get_prs {
+  state=${1}
+  paginate_arg=${2}
+  gh api ${paginate_arg} -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "/repos/easyjet-dev/ejh-web-digital/pulls?state=${state}&per_page=100"
 }
