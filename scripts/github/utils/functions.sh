c48edcd837a61ab07dda79d66ef27ac7007abf49
Abel Guillen <abelazo@gmail.com>
2025-02-06 17:21:03 +0100

    feat(github): Show highlighted diff in the output

--- scripts/github/utils/functions.sh
+++ scripts/github/utils/functions.sh
@@ -13,5 +13,6 @@ function github::wait_for_workflow {
 function github::get_prs {
   state=${1}
   paginate_arg=${2}
-  gh api ${paginate_arg} -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "/repos/easyjet-dev/ejh-web-digital/pulls?state=${state}&per_page=100"
+  prs=$(gh api ${paginate_arg} -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "/repos/easyjet-dev/ejh-web-digital/pulls?state=${state}&per_page=100")
+  jq -r '.[] | [.number, .title, .state, .created_at, .closed_at, .merged_at, if (.closed_at | length) != 0 then ((((.closed_at | strptime("%Y-%m-%dT%H:%M:%SZ") | mktime) - (.created_at | strptime("%Y-%m-%dT%H:%M:%SZ") | mktime)) / 3600) | floor) else null end] | @csv' <<<${prs}
 }
