4af245ea70a631b475e681e5625526fb94e386c0
Abel Guillen <abelazo@gmail.com>
2025-02-07 08:56:17 +0100

    feat(github): Add pagination

--- scripts/github/utils/functions.sh
+++ scripts/github/utils/functions.sh
@@ -14,7 +14,11 @@ function github::get_prs {
   local organization=${1}
   local repository=${2}
   local state=${3}
-  local paginate_arg=${4}
-  prs=$(gh api ${paginate_arg} -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "/repos/${organization}/${repository}/pulls?state=${state}&per_page=100")
-  jq -r '.[] | [.number, (.title | (gsub(",";""))), .state, .created_at, .closed_at, .merged_at, if (.closed_at | length) != 0 then ((((.closed_at | strptime("%Y-%m-%dT%H:%M:%SZ") | mktime) - (.created_at | strptime("%Y-%m-%dT%H:%M:%SZ") | mktime)) / 3600) | floor) else null end] | @csv' <<<${prs}
+  local pages=${4}
+  current_page=1
+  while [ ${current_page} -le ${pages} ]; do
+    prs=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "/repos/${organization}/${repository}/pulls?state=${state}&per_page=100&page=${current_page}")
+    jq -r '.[] | [.number, (.title | (gsub(",";""))), .state, .created_at, .closed_at, .merged_at, if (.closed_at | length) != 0 then ((((.closed_at | strptime("%Y-%m-%dT%H:%M:%SZ") | mktime) - (.created_at | strptime("%Y-%m-%dT%H:%M:%SZ") | mktime)) / 3600) | floor) else null end] | @csv' <<<${prs}
+    current_page=$((current_page+1))
+  done
 }
