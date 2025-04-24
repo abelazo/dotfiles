eaa6c7e2d6d1746fa67b552f3835570cc47b71d5
Abel Guillen <abelazo@gmail.com>
2025-04-24 17:33:10 +0200

    refactor(octopus): Move functionality to utils function

---
+++ scripts/octopus/utils/spaces.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+octopus::get_space() {
+  space_name=$1
+  if [[ "${space_name}" == "" ]]; then
+    space_id=$(http GET "https://${OCTOPUS_INSTANCE}.octopus.app/api/spaces?ApiKey=${API_KEY}" | jq -r '.Items[] | [.Name, .Id] | @csv' | gum table --columns "Name, Id" | cut -d ',' -f 2)
+  else
+    space_id=$(http GET "https://${OCTOPUS_INSTANCE}.octopus.app/api/spaces?ApiKey=${API_KEY}" | jq -r '.Items[] | select(.Name == "'${space_name}'") | .Id')
+  fi
+  echo ${space_id}
+}
