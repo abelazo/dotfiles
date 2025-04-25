b93db542d23178adf92fd58d97fa3ecadd669b9e
Abel Guillen <abelazo@gmail.com>
2025-04-25 10:51:58 +0200

    refactor(octopus): Improve readability

--- scripts/octopus/utils/spaces.sh
+++ scripts/octopus/utils/spaces.sh
@@ -3,9 +3,9 @@
 octopus::get_space() {
   space_name=$1
   if [[ "${space_name}" == "" ]]; then
-    space_id=$(http GET "https://${OCTOPUS_INSTANCE}.octopus.app/api/spaces?ApiKey=${API_KEY}" | jq -r '.Items[] | [.Name, .Id] | @csv' | gum table --columns "Name, Id" | cut -d ',' -f 2)
+    space_id=$(http GET "https://${OCTOPUS_INSTANCE}.octopus.app/api/spaces" ApiKey==${API_KEY} | jq -r '.Items[] | [.Name, .Id] | @csv' | gum table --columns "Name, Id" | cut -d ',' -f 2)
   else
-    space_id=$(http GET "https://${OCTOPUS_INSTANCE}.octopus.app/api/spaces?ApiKey=${API_KEY}" | jq -r '.Items[] | select(.Name == "'${space_name}'") | .Id')
+    space_id=$(http GET "https://${OCTOPUS_INSTANCE}.octopus.app/api/spaces" ApiKey==${API_KEY} | jq -r '.Items[] | select(.Name == "'${space_name}'") | .Id')
   fi
   echo ${space_id}
 }
