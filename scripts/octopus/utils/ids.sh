f2d868a44b50243c7baa4a7fa13ef869d26bcb3a
Abel Guillen <abelazo@gmail.com>
2025-05-08 17:23:59 +0200

    feat(octopus): Add Project and Channel to release menu

--- scripts/octopus/utils/ids.sh
+++ scripts/octopus/utils/ids.sh
@@ -24,7 +24,17 @@ octopus::get_release_id() {
   space_id=$1
   release_version=$2
   if [[ "${release_version}" == "" ]]; then
-    release_id=$(http GET "https://${OCTOPUS_INSTANCE}.octopus.app/api/spaces/${space_id}/releases" ApiKey==${API_KEY} | jq -r '.Items[] | [.Version, .Id] | @csv' | gum table --columns "Version, Id" --return-column 2)
+    all_channels=$(http --print b GET "https://${OCTOPUS_INSTANCE}.octopus.app/api/spaces/${space_id}/channels" ApiKey==${API_KEY})
+    all_projects=$(http --print b GET "https://${OCTOPUS_INSTANCE}.octopus.app/api/spaces/${space_id}/projects" ApiKey==${API_KEY})
+    all_releases=$(http GET "https://${OCTOPUS_INSTANCE}.octopus.app/api/spaces/${space_id}/releases" ApiKey==${API_KEY})
+
+    release_id=$(jq -r -n --argjson channels "$all_channels" --argjson releases "$all_releases" --argjson projects "$all_projects" '
+      $releases.Items[] as $release |
+      $channels.Items[] as $channel |
+      $projects.Items[] as $project |
+      select($release.ChannelId == $channel.Id and $release.ProjectId == $project.Id) |
+      [$project.Name, $channel.Name, $release.Version, $release.Id] |
+      @csv' | gum table --columns "Project,Channel,Version,Id" --return-column 4)
   else
     release_id=$(http GET "https://${OCTOPUS_INSTANCE}.octopus.app/api/spaces/${space_id}/releases" ApiKey==${API_KEY} | jq -r '.Items[] | select(.Version == "'${release_version}'") | .Id')
   fi
@@ -40,4 +50,11 @@ octopus::get_environment_id() {
     environment_id=$(http GET "https://${OCTOPUS_INSTANCE}.octopus.app/api/spaces/${space_id}/environments" ApiKey==${API_KEY} | jq -r '.Items[] | select(.Name == "'${environment_name}'") | .Id')
   fi
   echo ${environment_id}
+}
+
+octopus::get_projec_name() {
+  space_id=$1
+  project_id=$2
+  project_name=$(http GET "https://${OCTOPUS_INSTANCE}.octopus.app/api/spaces/${space_id}/projects" ApiKey==${API_KEY} | jq -r '.Items[0] | select(.Id == "'${project_id}'").Name')
+  echo ${project_name}
 }
