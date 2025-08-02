288178ec864129dd654a9ff384091568e1eaebe8
Abel Guillen <abelazo@gmail.com>
2025-08-02 16:13:07 +0200

    refactor(xbar): Simplify copy photos script

--- scripts/xbar/copy_photos.sh
+++ scripts/xbar/copy_photos.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # <bitbar.title>Copy CF/SD photos</bitbar.title>
 # <bitbar.author>Abel Guillen (abelazo@gmail.com)</bitbar.author>
@@ -9,67 +9,50 @@
 # Hack for language not being set properly and unicode support
 export LANG="${LANG:-en_US.UTF-8}"
 
+d300_volume="/Volumes/NIKON D300S"
+d300_folder="301D300S"
+
+z6_volume="/Volumes/NIKON Z 6_2"
+z6_folder="100NZ6_2"
+
 function copy {
-  source_folder=${1}
-  target_folder=${2}
-  mv "${source_folder}" "${target_folder}"
-  chmod 755 "${target_folder}"
-  chmod 644 "${target_folder}/*"
+  camera=${1}
+  card=${2}
+
+  folder=$d300_folder
+  volume=$d300_volume
+  [[ "$camera" == "Z6" ]] && folder=$z6_folder && volume=$z6_volume
+
+  source_folder="${volume}/DCIM/${folder}"
+  target_folder="$HOME/Desktop/${folder}_${card}"
+
+  if [[ -d "$source_folder" ]]; then
+    mv "$source_folder" "$target_folder"
+    chmod 755 "$target_folder"
+    chmod 644 "$target_folder/*"
+
+    diskutil eject "$volume/"
+
+    notification="$camera photos from $card card copied to ${folder}_${card}"
+    title="$camera photos"
+    subtitle="$card card"
+    osascript -e "display notification \"$notification\" with title \"$title\" subtitle \"$subtitle\""
+  else
+    notification="Memory card not mounted. Nothing to copy from"
+    title="$camera photos"
+    subtitle="$card card"
+    osascript -e "display notification \"$notification\" with title \"$title\" subtitle \"$subtitle\""
+  fi
 }
 
-d300_source_folder="/Volumes/NIKON D300S/DCIM/301D300S"
-
-if [[ "$1" = "D300_CF" ]]; then
-  target_folder="$HOME/Desktop/301D300S_CF"
-  if [ -d "${d300_source_folder}" ]; then
-    copy "${d300_source_folder}" "${target_folder}"
-    diskutil eject "/Volumes/NIKON D300S/"
-    osascript -e 'display notification "Memory card photos copied to '${target_folder}'" with title "Copy photos" subtitle "CF card"'
-  else
-    osascript -e 'display notification "CF memory card not mounted.\nNothing to copy" with title "Copy photos" subtitle "CF card"'
-  fi
+if [[ "$1" == "D300" || "$1" == "Z6" ]]; then
+  copy "$1" "$2"
 fi
-
-if [[ "$1" = "D300_SD" ]]; then
-  target_folder="$HOME/Desktop/301D300S_SD"
-  if [ -d "${d300_source_folder}" ]; then
-    copy "${d300_source_folder}" "${target_folder}"
-    diskutil eject "/Volumes/NIKON D300S/"
-    osascript -e 'display notification "Memory card photos copied to '${target_folder}'" with title "Copy photos" subtitle "SD card"'
-  else
-    osascript -e 'display notification "SD memory card not mounted.\nNothing to copy" with title "Copy photos" subtitle "SD card"'
-  fi
-fi
-
-z6_source_folder="/Volumes/NIKON Z 6_2/DCIM/100NZ6_2"
-
-if [[ "$1" = "Z6_CF" ]]; then
-  target_folder="$HOME/Desktop/100NZ6_2_CF"
-  if [ -d "${z6_source_folder}" ]; then
-    copy "${z6_source_folder}" "${target_folder}"
-    diskutil eject "/Volumes/NIKON Z 6_2/"
-    osascript -e 'display notification "Memory card photos copied to '${target_folder}'" with title "Copy photos" subtitle "CF card"'
-  else
-    osascript -e 'display notification "CF memory card not mounted.\nNothing to copy" with title "Copy photos" subtitle "CF card"'
-  fi
-fi
-
-if [[ "$1" = "Z6_SD" ]]; then
-  target_folder="$HOME/Desktop/100NZ6_2_SD"
-  if [ -d "${z6_source_folder}" ]; then
-    copy "${z6_source_folder}" "${target_folder}"
-    diskutil eject "/Volumes/NIKON Z 6_2/"
-    osascript -e 'display notification "Memory card photos copied to '${target_folder}'" with title "Copy photos" subtitle "SD card"'
-  else
-    osascript -e 'display notification "SD memory card not mounted.\nNothing to copy" with title "Copy photos" subtitle "SDF card"'
-  fi
-fi
-
 
 echo '📸'
 echo "---"
 
-echo "Copy D300 CF| bash='$0' param1=D300_CF terminal=false refresh=true"
-echo "Copy D300 SD| bash='$0' param1=D300_SD terminal=false refresh=true"
-echo "Copy Z6 CF| bash='$0' param1=Z6_CF terminal=false refresh=true"
-echo "Copy Z6 SD| bash='$0' param1=Z6_SD terminal=false refresh=true"
+echo "D300 - CF| bash='$0' param1=D300 param2=CF terminal=false refresh=true"
+echo "D300 - SD| bash='$0' param1=D300 param2=SD terminal=false refresh=true"
+echo "Z6 - CF| bash='$0' param1=Z6 param2=CF terminal=false refresh=true"
+echo "Z6 - SD| bash='$0' param1=Z6 param2=SD terminal=false refresh=true"
