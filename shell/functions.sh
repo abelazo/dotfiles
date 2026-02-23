ccd1dd07337407e220fc83f0ef13617bd699687d
Abel Guillen <abelazo@gmail.com>
2026-02-23 16:53:18 +0100

    feat(shell): Add sensitive function replacing unused portainer one

--- shell/functions.sh
+++ shell/functions.sh
@@ -22,9 +22,8 @@ function ttd() {
   gdate -d @$(( ($1 + 500) / ${divisor} ))
 }
 
-function portainer() {
-  docker volume create portainer_data
-  docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+function sensitive() {
+  echo "${1:0:4}***${1: -4}"
 }
 
 function cd() {
